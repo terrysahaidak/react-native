c73b7c23fff181d450d88efed0050e2b85e1af2e
Christian Falch <christian@framx.no>
2019-12-13 15:47:09 +0100

    [android] Added support for animating all props with useNativeDriver
    - Added support for configuration of allowed properties from js
    - Fixed color interpolation in interpolation node
    - Added support for differentiating props in style and prop nodes
    - Made member getUIViewOperationQueue public in UIManager to be able to test if we are currently updating our view so we don't call dispatchViewUpdates to often.

--- ReactAndroid/src/main/java/com/facebook/react/animated/NativeAnimatedModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/animated/NativeAnimatedModule.java
@@ -16,6 +16,7 @@ import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.ReactConstants;
@@ -29,6 +30,8 @@ import com.facebook.react.uimanager.UIBlock;
 import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.UIManagerModuleListener;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Module that exposes interface for creating and managing animated nodes on the "native" side.
@@ -192,7 +195,10 @@ public class NativeAnimatedModule extends ReactContextBaseJavaModule
 
       if (reactApplicationContext != null) {
         UIManagerModule uiManager = reactApplicationContext.getNativeModule(UIManagerModule.class);
-        mNodesManager = new NativeAnimatedNodesManager(uiManager);
+        mNodesManager = new NativeAnimatedNodesManager(
+          uiManager,
+          getReactApplicationContext()
+        );
       }
     }
 
@@ -214,6 +220,22 @@ public class NativeAnimatedModule extends ReactContextBaseJavaModule
   @VisibleForTesting
   public void setNodesManager(NativeAnimatedNodesManager nodesManager) {
     mNodesManager = nodesManager;
+  }
+
+  @ReactMethod
+  public void configureProps(ReadableArray shadowViewProps) {
+    int size = shadowViewProps.size();
+    final Set<String> props = new HashSet<>(size);
+    for (int i = 0; i < size; i++) {
+      props.add(shadowViewProps.getString(i));
+    }
+
+    mOperations.add(new UIThreadOperation() {
+      @Override
+      public void execute(NativeAnimatedNodesManager nodesManager) {
+        nodesManager.configureProps(props);
+      }
+    });
   }
 
   @ReactMethod
